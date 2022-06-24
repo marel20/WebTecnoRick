8a3299cab6d1dbee4cff298ed6badeaf31237c4d
marel20 <marelli_11@hotmail.com>
2022-06-24 00:00:35 -0300

    Modificaciones imagenes, textos

--- js/script.js
+++ js/script.js
@@ -31,3 +31,20 @@
     })
     /*----------End Btn Wsp---------*/
     
+    /*---------- Start Picture Galery --------*/
+   $(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+
+  /*-------- End Picture Galery ----------*/
